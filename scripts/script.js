eff2730405f0abc559e5cf119e7e20e28dcd4d8f
Vera Pervushina <vera.pervushina2608@gmail.com>
2023-06-29 23:43:08 +0700

    before

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,20 @@
-const popoverAuthBtn = document.querySelector('.popover-auth-button');
+/*const popoverAuthBtn = document.querySelector('.popover-auth-button');
 const authorisedUser = document.querySelector('.login-logout-wrapper');
 const openClass = document.querySelector('.open');
 const entryLink = document.querySelector('.entry-link-wrapper');
 const logout = document.querySelector('.wellcome-logout-button');
 const closeBtn = document.querySelector('.wellcome-close-button');
 const logoutLink = document.querySelector('.logout-link');
+
+const error = [...document.querySelectorAll('.error')];
+const errorText = [...document.querySelectorAll('.error-text')];
+
+error.forEach((element) => {
+console.log(element.nextElementSibling);
+if (element) {
+  element.nextElementSibling.style = "display: inline";
+}
+});
 
 
 popoverAuthBtn.addEventListener('click',() => {
@@ -20,4 +30,4 @@ logout.addEventListener('click', () => {
 logoutLink.addEventListener('click', () => {
   authorisedUser.classList.remove('open');
   entryLink.classList.add('open');
-});
+});*/
